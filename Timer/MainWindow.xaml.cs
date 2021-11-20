d0d35aebd704016624331e3b2f9bfa95b369b679
Hakan Fıstık <hakam.fstk@gmail.com>
2021-11-20 21:41:11 +0300

    Update to .net 6 implicit usings file scoped namespace

--- Timer/MainWindow.xaml.cs
+++ Timer/MainWindow.xaml.cs
@@ -1,42 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
 
-namespace Timer
+namespace Timer;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window, INotifyPropertyChanged
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    #region INotifyPropertyChanged
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void RaisePropertyChanged([CallerMemberName] string propertyName = null) =>
+        OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+
+
+    protected virtual void OnPropertyChanged(PropertyChangedEventArgs args) =>
+        PropertyChanged?.Invoke(this, args);
+
+    #endregion
+
+    public MainWindow()
     {
-        #region INotifyPropertyChanged
+        InitializeComponent();
+        DataContext = this;
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null) =>
-            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
-
-
-        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args) =>
-            PropertyChanged?.Invoke(this, args);
-
-        #endregion
-
-        public MainWindow()
-        {
-            InitializeComponent();
-            DataContext = this;
-
-            MidnightNotifier.DayChanged += (s, e) => RaisePropertyChanged(nameof(Days));
-        }
-
-        public double Days =>
-            (new DateTime(2023, 1, 1).Date - DateTime.Now.Date).TotalDays;
-
-        private void Button_Click(object sender, RoutedEventArgs e) =>
-            RaisePropertyChanged(nameof(Days));
-
+        MidnightNotifier.DayChanged += (s, e) => RaisePropertyChanged(nameof(Days));
     }
+
+    public double Days =>
+        (new DateTime(2023, 1, 1).Date - DateTime.Now.Date).TotalDays;
+
+    private void Button_Click(object sender, RoutedEventArgs e) =>
+        RaisePropertyChanged(nameof(Days));
 }
--- Timer/MidnightNotifier.cs
+++ Timer/MidnightNotifier.cs
@@ -1,44 +1,42 @@
 ﻿using Microsoft.Win32;
-using System;
 using T = System.Timers;
 
-namespace Timer
+namespace Timer;
+
+//https://stackoverflow.com/a/8480218
+static class MidnightNotifier
 {
-    //https://stackoverflow.com/a/8480218
-    static class MidnightNotifier
+    private static readonly T.Timer timer;
+
+    static MidnightNotifier()
     {
-        private static readonly T.Timer timer;
-
-        static MidnightNotifier()
+        timer = new T.Timer(GetSleepTime());
+        timer.Elapsed += (s, e) =>
         {
-            timer = new T.Timer(GetSleepTime());
-            timer.Elapsed += (s, e) =>
-            {
-                OnDayChanged();
-                timer.Interval = GetSleepTime();
-            };
-            timer.Start();
-
-            SystemEvents.TimeChanged += OnSystemTimeChanged;
-        }
-
-        private static double GetSleepTime()
-        {
-            var midnightTonight = DateTime.Today.AddDays(1);
-            var differenceInMilliseconds = (midnightTonight - DateTime.Now).TotalMilliseconds;
-            return differenceInMilliseconds;
-        }
-
-        private static void OnDayChanged()
-        {
-            DayChanged?.Invoke(null, null);
-        }
-
-        private static void OnSystemTimeChanged(object sender, EventArgs e)
-        {
+            OnDayChanged();
             timer.Interval = GetSleepTime();
-        }
+        };
+        timer.Start();
 
-        public static event EventHandler<EventArgs> DayChanged;
+        SystemEvents.TimeChanged += OnSystemTimeChanged;
     }
+
+    private static double GetSleepTime()
+    {
+        DateTime midnightTonight = DateTime.Today.AddDays(1);
+        double differenceInMilliseconds = (midnightTonight - DateTime.Now).TotalMilliseconds;
+        return differenceInMilliseconds;
+    }
+
+    private static void OnDayChanged()
+    {
+        DayChanged?.Invoke(null, EventArgs.Empty);
+    }
+
+    private static void OnSystemTimeChanged(object sender, EventArgs e)
+    {
+        timer.Interval = GetSleepTime();
+    }
+
+    public static event EventHandler<EventArgs> DayChanged;
 }
